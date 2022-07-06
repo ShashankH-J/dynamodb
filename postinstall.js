9cc0273095d7f011056f2cdd65829d1f6dddc294
Shashank H <83278687+ShashankH-J@users.noreply.github.com>
2022-07-06 11:12:10 +0530

    uncommented things in postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -5,24 +5,23 @@ const LIBRARY_NAME = process.env.npm_package_name;
 const EXEC_DIR = process.argv.slice(2)[process.argv.slice(2).indexOf("--init-dir") + 1];
 
 const initEnv = async () => {
-  console.log("running post-install ", LIBRARY_NAME, __dirname)
-  // let envFileContents = fs.readFileSync(path.resolve(EXEC_DIR, `${LIBRARY_NAME}/env.json`), 'utf8');
-  // let projectEnvFileContents = fs.readFileSync(path.resolve(EXEC_DIR, `src/config/config.json`), 'utf8');
-  // envFileContents = JSON.parse(envFileContents);
-  // projectEnvFileContents = JSON.parse(projectEnvFileContents);
+  let envFileContents = fs.readFileSync(path.resolve(EXEC_DIR, `${LIBRARY_NAME}/env.json`), 'utf8');
+  let projectEnvFileContents = fs.readFileSync(path.resolve(EXEC_DIR, `src/config/config.json`), 'utf8');
+  envFileContents = JSON.parse(envFileContents);
+  projectEnvFileContents = JSON.parse(projectEnvFileContents);
 
-  // // If public package then read other than organization name key
-  // const configKey = LIBRARY_NAME.split('/')[1].toUpperCase();
-  // if (projectEnvFileContents[configKey]) {
-  //   Object.keys(envFileContents).map(key => {
-  //     if (!projectEnvFileContents[configKey][key])
-  //       projectEnvFileContents[configKey][key] = envFileContents[key];
-  //   });
-  // } else {
-  //   projectEnvFileContents[configKey] = envFileContents;
-  // }
+  // If public package then read other than organization name key
+  const configKey = LIBRARY_NAME.split('/')[1].toUpperCase();
+  if (projectEnvFileContents[configKey]) {
+    Object.keys(envFileContents).map(key => {
+      if (!projectEnvFileContents[configKey][key])
+        projectEnvFileContents[configKey][key] = envFileContents[key];
+    });
+  } else {
+    projectEnvFileContents[configKey] = envFileContents;
+  }
 
-  // fs.writeFileSync(path.resolve(EXEC_DIR, `src/config/config.json`), JSON.stringify(projectEnvFileContents, null, 2), 'utf8');
+  fs.writeFileSync(path.resolve(EXEC_DIR, `src/config/config.json`), JSON.stringify(projectEnvFileContents, null, 2), 'utf8');
 }
 
 initEnv();
